960de9da94d0741f22440633bf95be3f3ab64948
John Kim <abcdefghijohnk@gmail.com>
2020-08-26 09:42:33 -0600

    create get price per square foot test

--- test/house_test.rb
+++ test/house_test.rb
@@ -66,6 +66,21 @@ class HouseTest < Minitest::Test
   def test_it_has_details
     house = House.new("$400000", "123 sugar lane")
 
-    assert_equal [price: 400000, address: "123 sugar lane"], house.details 
+    assert_equal [price: 400000, address: "123 sugar lane"], house.details
+  end
+
+  def test_it_can_get_price_per_square_foot
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+
+    house.add_room(room_4)
+    house.add_room(room_1)
+    house.add_room(room_3)
+    house.add_room(room_2)
+
+    assert_equal 210.53, house.price_per_square_foot 
   end
 end

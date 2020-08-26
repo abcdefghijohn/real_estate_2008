a1723dfa9916b782c55d82aaf6202b0f39a74bdf
John Kim <abcdefghijohnk@gmail.com>
2020-08-26 09:09:26 -0600

    check if room is painted test pass

--- lib/room.rb
+++ lib/room.rb
@@ -5,9 +5,18 @@ class Room
     @category = category
     @length = length
     @width = width
+    @painted = false
   end
 
   def area
     @length * @width.to_i
+  end
+
+  def paint
+    @painted = true
+  end
+
+  def is_painted?
+    @painted 
   end
 end
